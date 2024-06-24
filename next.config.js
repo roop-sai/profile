d1cb520e6058900f68c918a94791ae793c3e3e22
roop-sai <roop-sai>
2024-06-24 06:48:23 +0000

    Pushed from Locofy Builder

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    reactStrictMode: true,
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-  }
+const nextConfig = {
+  reactStrictMode: true,
+};
 
-  module.exports = nextConfig
+module.exports = nextConfig;
